0cdee10726d8b8d7c1ba16cfe344adaaa5c870cd
aartipandita <aartipandita>
2026-01-15 19:04:25 +0530

    Update RootLayout

--- app/layout.js
+++ app/layout.js
@@ -15,16 +15,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider
-      appearance={{
-        baseTheme: dark,
-      }}
-    >
+    <ClerkProvider appearance={{ baseTheme: dark }}>
       <html lang="en" suppressHydrationWarning>
         <head>
           <link rel="icon" href="/logo.png" sizes="any" />
         </head>
-        <body className={`${inter.className}`}>
+        <body className={`${inter.className} bg-white dark:bg-black text-gray-900 dark:text-gray-200`}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
@@ -34,10 +30,9 @@ export default function RootLayout({ children }) {
             <Header />
             <main className="min-h-screen">{children}</main>
             <Toaster richColors />
-
             <footer className="bg-muted/50 py-12">
-              <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by RoadsideCoder</p>
+              <div className="container mx-auto px-4 text-center text-gray-900 dark:text-gray-200 text-sm">
+                © 2026 Aarti Pandita. All rights reserved.
               </div>
             </footer>
           </ThemeProvider>
